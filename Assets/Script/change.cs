adc5a20fd26d2a12ac55b2ab22a5ae75a76b32bd
機関車ヘイヘイ\vantan <yu.love5416@gmail.com>
2021-09-24 02:34:19 +0900

    music.se

--- Assets/Script/change.cs
+++ Assets/Script/change.cs
@@ -6,9 +6,27 @@ using UnityEngine.SceneManagement;
 public class change : MonoBehaviour
 {
     [SerializeField] string m_scenename = null;
-    public void Botom(string name)
+    [SerializeField] float m_time = 1;
+
+    public void EndingGame()
     {
-        SceneManager.LoadScene(name);
+        //3秒後にメソッドを実行する
+        Invoke("LoadEndingScene", m_time);
     }
-    
+
+    void LoadEndingScene()
+    {
+        SceneManager.LoadScene(m_scenename);
+    }
 }
+   /* public void Botom(string name)
+    {
+        SE();
+    }
+
+    IEnumerator SE()
+    {
+        yield return new WaitForSeconds(m_time);
+        SceneManager.LoadScene(m_scenename);
+    }
+}*/
