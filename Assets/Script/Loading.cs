53cc931c39c08c6dadd4e3507bb2abb77495cbf3
機関車ヘイヘイ\vantan <yu.love5416@gmail.com>
2021-10-27 19:05:43 +0900

    DOTween とラムダ式

--- Assets/Script/Loading.cs
+++ Assets/Script/Loading.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class Loading : MonoBehaviour
 {
@@ -11,26 +12,41 @@ public class Loading : MonoBehaviour
 	[SerializeField]
 	private Slider m_slider;
 	[SerializeField] int m_reset = 0;
+	[SerializeField] float m_chageTime = 0.5f;
 
 
 	private void Start()
 	{
 		m_slider = GameObject.Find("Slider").GetComponent<Slider>();
 	}
-	public void NextScene()
-	{
-		StartCoroutine("Load");
-	}
+	//public void NextScene()
+	//{
+	//	StartCoroutine("Load");
+	//}
 
 	float m_time;
 	void Update()
 	{
-		m_time += Time.deltaTime;
-		m_slider.value = m_time;
+		//m_time += Time.deltaTime;
+		//m_slider.value = m_time;
 	}
-	IEnumerator Load()
+
+	public void Change(float value)
+    {
+		ChangeValue(m_slider.value + value);
+    }
+
+	public void Fill()
 	{
-		yield return new WaitForSeconds(m_reset);
-		SceneManager.LoadSceneAsync("Main");
+		ChangeValue(1f);
 	}
+
+	public void ChangeValue(float value)
+    {
+		DOTween.To(() => m_slider.value, x => m_slider.value = x, value, m_chageTime).OnComplete(() =>SceneManager.LoadSceneAsync("Main")); ;
+    }
+	//IEnumerator Load()
+	//{
+	//	yield return new WaitForSeconds(m_reset);
+	//}
 }
